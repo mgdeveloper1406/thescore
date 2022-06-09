f3758c261e9f6331ec32256279e8f4a5465db0da
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2022-06-09 12:20:39 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ function chillHop() {
         {
             name:"Sábado 04 de Junio",
             artist: "JOSÉ, EXPERTO EN SUEÑOS",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-SABADO.mp3?_=1",
@@ -15,7 +15,7 @@ function chillHop() {
         {
             name:"Domingo 05 de Junio",
             artist: "PROBLEMAS FAMILIARES",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-DOMINGO.mp3?_=2",
@@ -24,7 +24,7 @@ function chillHop() {
         {
             name:"Lunes 06 de Junio",
             artist: "EL ATAQUE A JOSÉ",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-LUNES.mp3?_=3",
@@ -33,7 +33,7 @@ function chillHop() {
         {
             name:"Martes 07 de Junio",
             artist: "JUDÁ Y TAMAR",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#008781", "#cfc08c"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-MARTES.mp3?_=4",
@@ -42,7 +42,7 @@ function chillHop() {
         {
             name:"Miércoles 08 de Junio",
             artist: "JOSÉ, ESCLAVO EN EGIPTO",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#845648","#FDFBDF"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-MIERCOLES.mp3?_=5",
@@ -51,7 +51,7 @@ function chillHop() {
         {
             name:"Jueves 09 de Junio",
             artist: "LOS SUEÑOS DE FARAÓN",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#578CCE", "#E9D6D0"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-JUEVES.mp3?_=6",
@@ -60,7 +60,7 @@ function chillHop() {
         {
             name:"Jueves 09 de Junio",
             artist: "Nymano, JK the Sage",
-            cover: "https://escuelasabatica.cl/2022/tri2/imagenes/tapa.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#191516", "#82698E"],
             audio: "https://mp3.chillhop.com/serve.php/?mp3=10130",
@@ -69,7 +69,7 @@ function chillHop() {
         {
             name:"Viernes 10 de Junio",
             artist: "PARA ESTUDIAR Y MEDITAR",
-            cover: "https://chillhop.com/wp-content/uploads/2020/07/cd6ad456b39b442e4d39e346c5f778df969326e6-1024x1024.jpg",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/03/leccion-escuela-sabatica-segundo-trimestre-2022-el-genesis-1.jpg",
             id: uuidv4(),
             color: ["#7BC8DD", "#D64A59"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2022/03/LECCION-11-VIERNES.mp3?_=7",
