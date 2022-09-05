1d31ba8556ff9e6231c3facaf94c0fac42e58402
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2022-09-05 10:58:06 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -9,10 +9,20 @@ function chillHop() {
             cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/06/en-el-crisol-con-cristo-guia-escuela-sabatica-3er-trimestre-pdf-1024x751.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
-            audio: "https://archive.org/serve/y-2mate.com-pr-bullon-leccion-10-templanza-en-el-crisol/y2mate.com%20-%20Pr%20Bull%C3%B3n%20%20Lecci%C3%B3n%2010%20%20Templanza%20en%20el%20crisol.mp3",
+            audio: "https://archive.org/serve/y-2mate.com-pr-bullon-leccion-11-aguardar-en-el-crisol/y2mate.com%20-%20Pr%20Bull%C3%B3n%20%20Lecci%C3%B3n%2011%20%20Aguardar%20en%20el%20crisol.mp3",
             active: true,       
          },
         {   
+            name:"Sábado 3 de septiembre",
+            artist: "Lección 11 – Aguardar en el Crisol",
+            cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/06/en-el-crisol-con-cristo-guia-escuela-sabatica-3er-trimestre-pdf-1024x751.jpg",
+            id: uuidv4(),
+            color: ["#D7B0D5", "#CBDFE0"],
+            audio: "https://escuelasabatica.cl/2022/tri3/lecc11/LECCION-11-SABADO.mp3",
+            active: false,
+        },
+        {
+            
             name:"Domingo 4 de septiembre",
             artist: "Lección 11 – Aguardar en el Crisol",
             cover: "https://www.recursos-biblicos.com/wp-content/uploads/2022/06/en-el-crisol-con-cristo-guia-escuela-sabatica-3er-trimestre-pdf-1024x751.jpg",
